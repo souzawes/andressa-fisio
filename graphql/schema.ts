aeb11094d7af5360989dfdbca477df7b5647f13a
Wesley Souza <souzawes93@gmail.com>
2024-05-26 18:18:24 -0300

    🐛 fix: removing graphql library that gives error when imported

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,22 +1,22 @@
-import { gql } from 'apollo-server-micro';
+// import { gql } from 'apollo-server-micro';
 
-export const typeDefs = gql`
-    type Patients {
-        id: String
-        name: String
-        cpf: String
-        address: String
-        neighborhood: String
-        number_house: String
-        date_of_birth: Date
-        sex: String
-        civil_state: String
-        job: String
-    }
+// export const typeDefs = gql`
+//     type Patients {
+//         id: String
+//         name: String
+//         cpf: String
+//         address: String
+//         neighborhood: String
+//         number_house: String
+//         date_of_birth: Date
+//         sex: String
+//         civil_state: String
+//         job: String
+//     }
 
-    type Query {
-        patients: [Patients]!
-    }
+//     type Query {
+//         patients: [Patients]!
+//     }
 
     
-`;
+// `;
